72f06d5c5727311b7a33b59faf685a44ad794e09
Shreenidhi Hegde <shreenidhishegde@gmail.com>
2024-09-08 14:37:02 -0700

    updated requirement.txt

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 # exit on error
 set -o errexit
 python -m pip install --upgrade pip
-pip install wheel
+pip install wheel setuptools
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
