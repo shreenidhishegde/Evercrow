0a9dd8c3a8ad77e40bd447360b154bee7c036e0e
Shreenidhi Hegde <shreenidhishegde@gmail.com>
2024-09-08 23:23:30 -0700

    Add title field to PDFDocument with default value

--- birdcounter/models.py
+++ birdcounter/models.py
@@ -7,14 +7,15 @@ from django.contrib.auth.models import User
 #     file = models.FileField(upload_to='pdfs')
 #     upload_at = models.DateTimeField(auto_now_add=True)
     
+    
 class PDFDocument(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pdf_documents')
-    file = models.FileField(upload_to='pdfs')
-    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='pdfs/')
+    title = models.CharField(max_length=255, default='Untitled Document')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.title} - {self.user.username if self.user else 'No User'}"
+        return self.title
 
 class BirdCount(models.Model):
     document = models.ForeignKey(PDFDocument, on_delete=models.CASCADE, related_name='bird_counts')
