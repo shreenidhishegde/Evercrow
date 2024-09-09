f7dd4caebe4026012ef30ffb15d949a60c95df4f
Shreenidhi Hegde <shreenidhishegde@gmail.com>
2024-09-08 23:17:10 -0700

    Update build script and add migrations

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
+python -m pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
 
-# Make migrations (in case there are new changes)
-python manage.py makemigrations
+# Make migrations without asking for input
+python manage.py makemigrations --noinput
 
 # Apply migrations
 python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --no-input
-
-
-
