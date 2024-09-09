623692003ef2513e08cfa7ae2fdb543f4459dd25
Shreenidhi Hegde <shreenidhishegde@gmail.com>
2024-09-08 17:52:45 -0700

    build file updated

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-python -m pip install --upgrade pip
-pip install wheel setuptools
+# python -m pip install --upgrade pip
+# pip install wheel setuptools
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-gunicorn evercrow_project.wsgi:application
+
+
+
